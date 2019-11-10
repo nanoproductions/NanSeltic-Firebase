6d2ffb8e3bd38d72e1c42be4fb98c3eb3d236589
Nano Productions <razzakammar.edu@gmail.com>
2019-11-10 13:42:07 -0500

    addded files

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
-import LoginComponent from './Login/login';
-import SignupComponent from './Signup/signup';
-import DashboardComponent from './Dashboard/dashboard';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+import LoginComponent from "./Login/login";
+import SignupComponent from "./Signup/signup";
+import DashboardComponent from "./Dashboard/dashboard";
 
 const firebase = require("firebase");
 require("firebase/firestore"); // Required for side-effects?????
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAlWBvbvLv7dT6_RYnlCeZbOcotpeBU3Y8",
-  authDomain: "im-app-tutorial.firebaseapp.com",
-  databaseURL: "https://im-app-tutorial.firebaseio.com",
-  projectId: "im-app-tutorial",
-  storageBucket: "im-app-tutorial.appspot.com",
-  messagingSenderId: "199544684635",
-  appId: "1:199544684635:web:fb388e2c181f0476"
+  apiKey: "AIzaSyAZLTX1ts95q99gg5HlX5O9WtPpJ8glBeM",
+  authDomain: "nanseltic-fireba.firebaseapp.com",
+  databaseURL: "https://nanseltic-fireba.firebaseio.com",
+  projectId: "nanseltic-fireba",
+  storageBucket: "nanseltic-fireba.appspot.com",
+  messagingSenderId: "115605607305",
+  appId: "1:115605607305:web:a317b78c3b26cec1a84758"
 });
 
 const routing = (
   <Router>
-    <div id='routing-container'>
-      <Route path='/login' component={LoginComponent}></Route>
-      <Route path='/signup' component={SignupComponent}></Route>
-      <Route path='/dashboard' component={DashboardComponent}></Route>
+    <div id="routing-container">
+      <Route path="/login" component={LoginComponent}></Route>
+      <Route path="/signup" component={SignupComponent}></Route>
+      <Route path="/" exact component={DashboardComponent}></Route>
     </div>
   </Router>
 );
 
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
